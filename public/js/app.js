73e8fab6d61a0398a5a13acaf912e0973f60b5d7
Ido Kahlon <idokah@idokah-mbp.lan.wixpress.com>
2021-05-09 14:49:33 +0300

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,3 @@
-console.log('Client side javascript file is loaded!')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log('data: ', data);
-    })
-})
-
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -18,10 +9,10 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
 
     const location = search.value
-    messageOne.textContent='Loading ...'
-    messageTwo.textContent=''
+    messageOne.textContent = 'Loading ...'
+    messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
@@ -29,8 +20,8 @@ weatherForm.addEventListener('submit', (e) => {
             } else {
                 messageOne.textContent = data.location
                 messageTwo.textContent = data.forecast
-                console.log(data.location) 
-                console.log(data.forecast) 
+                console.log(data.location)
+                console.log(data.forecast)
             }
         })
     })
